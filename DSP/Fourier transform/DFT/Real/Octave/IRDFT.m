d67f55059f48ef1ab613647d076814a9f6adce5d
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-01-22 00:20:37 +0200

    Inverse Real DFT exmaple for Octave fixed

--- DSP/Fourier transform/DFT/Real/Octave/IRDFT.m
+++ DSP/Fourier transform/DFT/Real/Octave/IRDFT.m
@@ -3,7 +3,7 @@
 % Inverse Real DFT
 
 % Time domain length
-td_length = 16;
+td_length = 32;
 
 % Frequency domain length
 fd_length = (td_length/2)+1;
@@ -12,61 +12,53 @@ fd_length = (td_length/2)+1;
 X = zeros(1, td_length);
 
 % Prepare frequency domain (randomly taken data)
-ReX = [0,   1, 0,   2, 0, 0, 0, 0,   3];
-ImX = [0, 0.3, 0, 0.6, 0, 0, 0, 0, 0.9];
+ReX = ones(1, fd_length);
+ImX = zeros(1, fd_length);
 
 % Calculus
 % Finding the cosine and sine wave amplitudes
 
-ReX_ = ReX;
-ImX_ = ImX;
+t = double(td_length)/2;
 
-t = td_length/2;
+ReX_ = double(ReX)/t;
+ImX_ = double(ImX)/t;
 
-for k=1:length(fd_length),
-	ReX_(k) = ReX_(k)/t;
-	ImX_(k) = ImX_(k)/t;
-end;
+ReX_(1) = ReX_(1)/2;
+ReX_(length(ReX_)) = ReX_(length(ReX_))/2;
 
-ReX(1) = ReX(1)/2;
-ReX(fd_length) = ReX(fd_length)/2;
-
-t = 2*pi/td_length
+t = 2*pi/td_length;
 
 for k=1:fd_length,
-    for i=1:td_length,       
-        X(i) = X(i) + ReX_(k)*cos(t*k*i);
-    	X(i) = X(i) + ImX_(k)*sin(t*k*i);
+    for i=1:td_length,
+        X(i) = X(i) + ReX_(k)*cos(t*(k-1)*(i-1));
+    	X(i) = X(i) + ImX_(k)*sin(t*(k-1)*(i-1));
     end;
 end;
 
 % Displaying
-
 y_range_amplifier = 1.3;
 
 td = 0:fd_length-1;
 
 subplot(2, 2, 1);
-plot(td, ReX(1:length(ReX)), 'o');
-grid on
-y_range = max(abs(min(ReX)), abs(max(ReX)))*y_range_amplifier;
-axis([0 length(td)-1 -y_range y_range]);
+plot(td, ReX, 'o');
+grid on;
+axis([0 length(td)-1 -0.5 1.5]);
 title('Re X - Cosine waves amplitudes');
 ylabel('Value');
 
 subplot(2, 2, 2);
-plot(td, ImX(1:length(ImX)), 'o');
-grid on
-y_range = max(abs(min(ImX)), abs(max(ImX)))*y_range_amplifier;
-axis([0 length(td)-1 -y_range y_range]);
+plot(td, ImX, 'o');
+grid on;
+axis([0 length(td)-1 -0.5 1.5]);
 title('Im X - Sine waves amplitudes');
 ylabel('Value');
 
 td = 0:td_length-1;
 
 subplot(2, 2, 3:4);
-plot(td, X(1:length(X)), 'o');
-grid on
+plot(td, X, 'o');
+grid on;
 y_range = max(abs(min(X)), abs(max(X)))*y_range_amplifier;
 axis([0 length(td)-1 -y_range y_range]);
 title('X - Synthesized signal');
